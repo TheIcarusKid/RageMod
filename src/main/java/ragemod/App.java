ef6f5b76d7c31ee3331640b95118cc1f13d94065
BrokenTomato <joergen.g@gmail.com>
2011-06-15 19:41:50 +0200

    Test
    Signed-off-by: BrokenTomato <joergen.g@gmail.com>

---
+++ src/main/java/ragemod/App.java
@@ -0,0 +1,13 @@
+package ragemod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
